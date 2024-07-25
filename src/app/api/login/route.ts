188122bda6b0a0d1e33df2eaf228e199630a0416
Hardik Arora <70485045+hardikarora2311@users.noreply.github.com>
2024-07-25 13:59:44 +0530

    format

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server';
-import { encode_jwt } from 'jwt-library';
+import { NextResponse } from "next/server";
+import { encode_jwt } from "jwt-library";
 
-const secret = 'my-secret-key';
+const secret = "my-secret-key";
 
 export async function POST() {
-  const userId = '123';
-  const token = await encode_jwt(secret, userId, { username: 'example_user' });
+  const userId = "123";
+  const token = await encode_jwt(secret, userId, { username: "example_user" });
   return NextResponse.json({ token });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,51 +36,51 @@ export default function Home() {
   };
 
   return (
-      <main className="flex flex-col min-h-screen justify-between p-24 mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
-        <div className="w-full text-sm flex flex-col">
-          <h1 className="text-4xl font-bold mb-8 mx-auto">JWT Demo App</h1>
-          <div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
-              onClick={login}
-            >
-              Login
-            </button>
-            <button
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => fetchProtectedData(false)}
-            >
-              Fetch Protected Data
-            </button>
-          </div>
-
-          <div className="mt-4 ">
-            {" "}
-            <span className="font-semibold">Token: </span> {token}
-          </div>
-          <div className="mt-4">
-            {" "}
-            <span className="font-semibold">Protected Data: </span>
-            {protectedData}
-          </div>
-
-          <div className="mt-8">
-            <input
-              type="text"
-              value={manualToken}
-              onChange={(e) => setManualToken(e.target.value)}
-              placeholder="Paste token here"
-              className="border-2 border-gray-300 p-2 w-full text-zinc-700"
-            />
-            <button
-              className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-6"
-              onClick={() => fetchProtectedData(true)}
-            >
-              Test Manual Token
-            </button>
-          </div>
+    <main className="flex flex-col min-h-screen justify-between p-24 mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+      <div className="w-full text-sm flex flex-col">
+        <h1 className="text-4xl font-bold mb-8 mx-auto">JWT Demo App</h1>
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+            onClick={login}
+          >
+            Login
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => fetchProtectedData(false)}
+          >
+            Fetch Protected Data
+          </button>
         </div>
-        <Footer />
-      </main>
+
+        <div className="mt-4 ">
+          {" "}
+          <span className="font-semibold">Token: </span> {token}
+        </div>
+        <div className="mt-4">
+          {" "}
+          <span className="font-semibold">Protected Data: </span>
+          {protectedData}
+        </div>
+
+        <div className="mt-8">
+          <input
+            type="text"
+            value={manualToken}
+            onChange={(e) => setManualToken(e.target.value)}
+            placeholder="Paste token here"
+            className="border-2 border-gray-300 p-2 w-full text-zinc-700"
+          />
+          <button
+            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-6"
+            onClick={() => fetchProtectedData(true)}
+          >
+            Test Manual Token
+          </button>
+        </div>
+      </div>
+      <Footer />
+    </main>
   );
 }
